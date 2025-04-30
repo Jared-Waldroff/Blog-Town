e7b7cd927bfa45514ff4e9d6cd9114259fb96721
Jared Waldroff <96996261+Jared-Waldroff@users.noreply.github.com>
2025-04-29 17:06:38 -0700

    Updated KoinModule with user, blog and savedBlog repositories and services

---
+++ src/main/kotlin/com/example/blogtown/dependencyInjection/KoinModule.kt
@@ -0,0 +1,28 @@
+package com.example.com.example.blogtown.di
+
+
+import com.example.com.example.blogtown.domain.repository.BlogRepository
+import com.example.com.example.blogtown.domain.repository.SavedBlogRepository
+import com.example.com.example.blogtown.domain.repository.UserRepository
+import com.example.com.example.blogtown.domain.service.AuthService
+import com.example.com.example.blogtown.domain.service.BlogService
+import com.example.com.example.blogtown.domain.service.UserService
+import com.example.com.example.blogtown.persistence.repository.BlogRepositoryImpl
+import com.example.com.example.blogtown.persistence.repository.SavedBlogRepositoryImpl
+import com.example.com.example.blogtown.persistence.repository.UserRepositoryImpl
+import com.example.com.example.blogtown.services.AuthServiceImpl
+import com.example.com.example.blogtown.services.BlogServiceImpl
+import com.example.com.example.blogtown.services.UserServiceImpl
+import org.koin.dsl.module
+
+val appModule = module {
+    // Repositories
+    single<UserRepository> { UserRepositoryImpl() }
+    single<BlogRepository> { BlogRepositoryImpl() }
+    single<SavedBlogRepository> { SavedBlogRepositoryImpl() }
+
+    // Services
+    single<AuthService> { AuthServiceImpl(get()) }
+    single<UserService> { UserServiceImpl(get()) }
+    single<BlogService> { BlogServiceImpl(get(), get(), get()) }
+}
