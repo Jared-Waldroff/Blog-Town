a0e6eb9986168cab5d898e2ae2343331f8856a98
Jared Waldroff <96996261+Jared-Waldroff@users.noreply.github.com>
2025-04-30 18:08:22 -0700

    Updated koin module to properly inject dependencies

--- src/main/kotlin/com/example/blogtown/di/KoinModule.kt
+++ src/main/kotlin/com/example/blogtown/di/KoinModule.kt
@@ -1,6 +1,7 @@
 package com.example.com.example.blogtown.di
 
 
+import com.example.com.example.blogtown.config.JwtConfig
 import com.example.com.example.blogtown.domain.repository.BlogRepository
 import com.example.com.example.blogtown.domain.repository.SavedBlogRepository
 import com.example.com.example.blogtown.domain.repository.UserRepository
@@ -13,15 +14,25 @@ import com.example.com.example.blogtown.infrastructure.persistence.repository.Us
 import com.example.com.example.blogtown.services.AuthServiceImpl
 import com.example.com.example.blogtown.services.BlogServiceImpl
 import com.example.com.example.blogtown.services.UserServiceImpl
+import io.ktor.server.application.*
+import io.ktor.server.config.*
 import org.koin.dsl.module
+import org.slf4j.LoggerFactory
+
+fun createConfigModule(application: Application) = module {
+    single { JwtConfig(application.environment.config) }
+}
 
 val appModule = module {
-    // Repositories
+    single { LoggerFactory.getLogger("AppLogger") }}
+
+val repositoryModule = module {
     single<UserRepository> { UserRepositoryImpl() }
     single<BlogRepository> { BlogRepositoryImpl() }
     single<SavedBlogRepository> { SavedBlogRepositoryImpl() }
+}
 
-    // Services
+val serviceModule = module {
     single<AuthService> { AuthServiceImpl(get()) }
     single<UserService> { UserServiceImpl(get()) }
     single<BlogService> { BlogServiceImpl(get(), get(), get()) }
